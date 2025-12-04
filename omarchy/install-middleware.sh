c0d39eb3d0f8c322623dc7563d91489a5d2a50b1
odajima-ca <odajima_yu_gn@cyberagent.co.jp>
2025-12-04 18:27:45 +0900

    Simplify `install-middleware.sh`: replace `unless` with `if !` for consistency and readability.

--- omarchy/install-middleware.sh
+++ omarchy/install-middleware.sh
@@ -5,14 +5,14 @@ if ! [ -d "$ANYENV_DIR" ]; then
   git clone https://github.com/anyenv/anyenv "$ANYENV_DIR"
 fi
 
-unless [ -d "$HOME/.oh-my-zsh" ]; then
+if ! [ -d "$HOME/.oh-my-zsh" ]; then
   RUNZSH=no CHSH=no KEEP_ZSHRC=yes \
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 ZSH_CUSTOM_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
 P10K_DIR="$ZSH_CUSTOM_DIR/themes/powerlevel10k"
-unless [ -d "$P10K_DIR" ]; then
+if ! [ -d "$P10K_DIR" ]; then
   mkdir -p "$ZSH_CUSTOM_DIR/themes"
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$P10K_DIR"
 fi
