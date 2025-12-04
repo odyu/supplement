6e17da5fe3f1d2f11413bb3967788c9a960da6bc
odajima-ca <odajima_yu_gn@cyberagent.co.jp>
2025-12-04 18:39:44 +0900

    Remove `.p10k.zsh`: clean up unused Powerlevel10k configuration file.

--- omarchy/install-dotfiles.sh
+++ omarchy/install-dotfiles.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-cd $HOME/supplement/dotfiles
+set -euo pipefail
 
+cd "$HOME/supplement/dotfiles"
+
+# 配布（stow）
 stow -v -R --no-folding -t "$HOME" zsh
 stow -v -R --no-folding -t "$HOME" p10k
 stow -v -R --no-folding -t "$HOME" ideavim
